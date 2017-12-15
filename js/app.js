84947cd412bf4a99e3f1df3ddacb9ca0ffe8d4f6
MariacristinaOrtiz <mari_d17@hotmail.com>
2017-12-15 08:03:15 -0500

    Añade elementos iniciales

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
-// La funcionalidad de tu proyecto
+window.addEventListener('load', function(event){
+  console.log(event);/*me da propiedades del evento*/
+  console.log(event.target);/* recibe y ejecuta el evento me bota document*/
+  console.log(event.currentTarget);/* escucha el click (tiene el addeventlistener por default)el evento me bota window*/
+  /*escribir las funcione*/
+});
